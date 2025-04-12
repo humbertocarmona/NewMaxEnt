c877dc8bde7353d4528ab493e1ab48c227797822
Humberto Carmona <carmona@fisica.ufc.br>
2025-04-12 09:24:06 -0300

    exp_q to utilities

--- include/util/utilities.hpp
+++ include/util/utilities.hpp
@@ -1,35 +1,56 @@
 #pragma once
 
+#include <algorithm>
 #include <armadillo>
 #include <cmath>
-#include <string>
 #include <filesystem>
 #include <regex>
-#include <algorithm>
+#include <string>
 #include <vector>
 
-namespace utils {
-    double rms(const arma::Col<double>& vec1, const arma::Col<double>& vec2);
+namespace utils
+{
+double rms(const arma::Col<double> &vec1, const arma::Col<double> &vec2);
 
-    arma::Col<double> center_xy(const arma::Col<double> &x, const arma::Col<double> &xy);
+arma::Col<double> center_xy(const arma::Col<double> &x, const arma::Col<double> &xy);
 
-    arma::Col<double> center_xyz(const arma::Col<double> &x, const arma::Col<double> &xy, const arma::Col<double> &xyz);
-    
-    std::string today();
-    
-    std::string now_30();
-    
-    std::string now();
+arma::Col<double> center_xyz(const arma::Col<double> &x, const arma::Col<double> &xy, const arma::Col<double> &xyz);
 
-    void make_path(const std::filesystem::path &path);
-    
-    bool is_file(const std::filesystem::path &path);
-    
-    bool is_dir(const std::filesystem::path &path);
-    
-    int next_run_id(const std::string& directory, const std::regex& pattern);
+std::string today();
 
-    template <typename T>
-    std::string col_string(const arma::Col<T>& col);
+std::string now_30();
+
+std::string now();
+
+void make_path(const std::filesystem::path &path);
+
+bool is_file(const std::filesystem::path &path);
+
+bool is_dir(const std::filesystem::path &path);
+
+int next_run_id(const std::string &directory, const std::regex &pattern);
+
+template <typename T> std::string col_string(const arma::Col<T> &col);
+
+// Generalized exponential
+// and logarithm (Tsallis
+// q-statistics)
+// -------------------------------------------------------------
+inline double exp_q(double x, double q)
+{
+    if (q == 1.0)
+        return std::exp(x);
+    double y = 1.0 + (1.0 - q) * x;
+    if (y <= 0.0)
+        return 0.0;
+    return std::pow(y, 1.0 / (1.0 - q));
 }
 
+inline double log_q(double x, double q)
+{
+    if (q == 1.0)
+        return std::log(x);
+    return (std::pow(x, 1 - q) - 1.0) / (1.0 - q);
+}
+
+} // namespace utils

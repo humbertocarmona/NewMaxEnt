c56b83966a4acaee7998cfb331d7f948844411b6
Humberto Carmona <carmona@fisica.ufc.br>
2025-04-22 17:39:34 -0300

    baseTrainer not modifying input_run.json new parameters

--- src/trainers/base_trainer_constructor.cpp
+++ src/trainers/base_trainer_constructor.cpp
@@ -48,12 +48,13 @@ BaseTrainer::BaseTrainer(MaxEntCore &core,
     }
     else if (utils::isFileType(data_filename, "json"))
     {
+        logger->info("[BaseTrainer] I am here");
         // re-start from last run (more iterations)
         auto obj = readTrainedModel(data_filename);
         if (!obj.contains("type") || obj["type"] != className)
         {
             logger->error("Incorrect or missing object type in JSON");
-            throw std::runtime_error("Invalid JSON object type");
+            throw std::runtime_error("Not a BaseTrainer file");
         }
 
         int n = obj["run_parameters"]["nspins"];
@@ -62,21 +63,23 @@ BaseTrainer::BaseTrainer(MaxEntCore &core,
             logger->error("wrong number of spins {}, expected {} ", n, core.nspins);
             throw std::runtime_error("Wrong number of spins");
         }
-        iter        = obj["iter"].get<int>();
-        m1_data     = utils::jsonToArmaCol<double>(obj["m1_data"]);
-        m2_data     = utils::jsonToArmaCol<double>(obj["m2_data"]);
-        m3_data     = utils::jsonToArmaCol<double>(obj["m3_data"]);
-        core.h      = utils::jsonToArmaCol<double>(obj["h"]);
-        core.J      = utils::jsonToArmaCol<double>(obj["J"]);
-        q_val       = obj["run_parameters"]["q_val"];
-        tolerance_h = obj["run_parameters"]["tolerance_h"];
-        tolerance_J = obj["run_parameters"]["tolerance_J"];
-        eta_h       = obj["run_parameters"]["eta_h"];
-        eta_J       = obj["run_parameters"]["eta_J"];
-        alpha_h     = obj["run_parameters"]["alpha_h"];
-        alpha_J     = obj["run_parameters"]["alpha_J"];
-        gamma_h     = obj["run_parameters"]["gamma_h"];
-        gamma_J     = obj["run_parameters"]["gamma_J"];
+        iter    = obj["iter"].get<int>();
+        m1_data = utils::jsonToArmaCol<double>(obj["m1_data"]);
+        m2_data = utils::jsonToArmaCol<double>(obj["m2_data"]);
+        m3_data = utils::jsonToArmaCol<double>(obj["m3_data"]);
+        core.h  = utils::jsonToArmaCol<double>(obj["h"]);
+        core.J  = utils::jsonToArmaCol<double>(obj["J"]);
+        // will run with the new run_parameters
+        //
+        // q_val       = obj["run_parameters"]["q_val"];
+        // tolerance_h = obj["run_parameters"]["tolerance_h"];
+        // tolerance_J = obj["run_parameters"]["tolerance_J"];
+        // eta_h       = obj["run_parameters"]["eta_h"];
+        // eta_J       = obj["run_parameters"]["eta_J"];
+        // alpha_h     = obj["run_parameters"]["alpha_h"];
+        // alpha_J     = obj["run_parameters"]["alpha_J"];
+        // gamma_h     = obj["run_parameters"]["gamma_h"];
+        // gamma_J     = obj["run_parameters"]["gamma_J"];
     }
     else
     {
